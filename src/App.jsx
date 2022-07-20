b421aea6bd8f7c75ca4c9d7eb08d72461a1c9865
Rijal Islami <rijalislamir@gmail.com>
2022-07-20 23:00:52 +0700

    Add axios, create async thunk to get users from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { getUsers, selectUsers } from './store/userSlice'
 import './App.css'
 
 function App() {
+	const dispatch = useDispatch()
+	const users = useSelector(selectUsers)
+
+	useEffect(() => {
+		dispatch(getUsers())
+	}, [])
+
 	return (
 		<div>
-			Hello Worlds!
+			{users.map((user, i) => 
+				<div key={"user-"+i}>
+					{user.name}
+				</div>
+			)}
 		</div>
 	)
 }
---
+++ src/store/userSlice.js
@@ -0,0 +1,52 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import axios from "axios";
+
+export const getUsers = createAsyncThunk(
+    "user/getUsers", 
+    async () => {
+        try {
+            console.log("trying")
+            const response = await axios.get("https://jsonplaceholder.typicode.com/users")
+            return response.data
+        } catch (error) {
+            console.error(error)
+        }
+    }
+)
+
+const initialState = {
+    all: [],
+    isLoading: false,
+    isSuccess: false,
+    isError: false,
+    message: ""
+}
+
+export const userSlice = createSlice({
+    name: "user",
+    initialState,
+    extraReducers: (builder) => {
+        builder
+            .addCase(getUsers.fulfilled, (state, action) => {
+                state.all = action.payload
+                state.isLoading = false
+                state.isSuccess = true
+                state.isError = false
+            })
+            .addCase(getUsers.pending, (state, action) => {
+                state.isLoading = true
+                state.isSuccess = false
+                state.isError = false
+            })
+            .addCase(getUsers.rejected, (state, action) => {
+                state.isLoading = false
+                state.isSuccess = false
+                state.isError = true
+            })
+    }
+})
+
+// export const { getUsers } = userSlice.actions
+export const selectUsers = state => state.user.all
+
+export default userSlice.reducer
